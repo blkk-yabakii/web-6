deb7675a70396080e8a56aa5011bebf91fd1870c
tanpabatas198 <tanpabatas198@gmail.com>
2022-09-29 15:27:46 +0700

    update

--- js/app.js
+++ js/app.js
@@ -1,31 +1,37 @@
 const users = [
-  { name: "Imdadul Mungin", age: '20' },
-  { name: "Muhamad Rizki", age: '20' },
-  { name: "Muhammad Fikri", age: '20' },
-  { name: "Sidik Purnomo", age: '20' },
-  { name: "Ragil Waluyo", age: '20' },
-  { name: "Roy Rehan", age: '20' },
-  { name: "Abdulloh Muhamad Mutaqqin", age: '20' },
-  { name: "Wafa", age: '20' },
-  { name: "Vina Mk", age: '20' },
-  { name: "Tifrohana Tz", age: '20' },
-  { name: "Zulaikha", age: '20' },
-  { name: "Putri hera", age: '20' },
-  { name: "Evi K", age: '20' },
-  { name: "Wielia W", age: '20' },
-  { name: "Mella F", age: '20' },
-  { name: "Waridatul w", age: '20' },
+  { name: "Imdadul Mungin", age: "20" },
+  { name: "Muhamad Rizki", age: "20" },
+  { name: "Muhammad Fikri", age: "20" },
+  { name: "Sidik Purnomo", age: "20" },
+  { name: "Ragil Waluyo", age: "20" },
+  { name: "Roy Rehan", age: "20" },
+  { name: "Abdulloh Muhamad Mutaqqin", age: "20" },
+  { name: "Wafa", age: "20" },
+  { name: "Vina Mk", age: "20" },
+  { name: "Tifrohana Tz", age: "20" },
+  { name: "Zulaikha", age: "20" },
+  { name: "Putri hera", age: "20" },
+  { name: "Evi K", age: "20" },
+  { name: "Wielia W", age: "20" },
+  { name: "Mella F", age: "20" },
+  { name: "Waridatul w", age: "20" },
 ];
 
+const div = document.querySelector('body > div')
 
-console.log(duaKaliLipat(1) + kuadrat(1)); // 1
-console.log(duaKaliLipat(2) + kuadrat(2)); // 4
-console.log(duaKaliLipat(3) + kuadrat(3)); // 15
-console.log(duaKaliLipat(4) + kuadrat(4)); // 24
-console.log(duaKaliLipat(5) + kuadrat(5)); // 35
+const table = document.createElement('table')
+const tbody = document.createElement('tbody')
 
-// buat fungsi untuk menghitung
-// 1. angka dua kali lipat.
-// 2. kuadrat
-// 3. hasil dari 1 di tambah hasil dari 2
-// parameter: terserah
+for (let i = 0; i < users.length; ++i) {
+  const user = users[i];
+  const name = document.createElement('td')
+  name.appendChild(document.createTextNode(user.name))
+  const age = document.createElement('td')
+  age.appendChild(document.createTextNode(user.age))
+  const row = document.createElement('tr');
+  row.appendChild(name);
+  row.appendChild(age);
+  tbody.appendChild(row);
+}
+table.appendChild(tbody);
+div.appendChild(table);
