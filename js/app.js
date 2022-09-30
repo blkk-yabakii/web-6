90a037924dab50a07363b9a424bb7131e95d65b3
tanpabatas198 <tanpabatas198@gmail.com>
2022-09-30 13:53:28 +0700

    update

--- js/app.js
+++ js/app.js
@@ -1,11 +1,22 @@
 const input = document.getElementById("isian");
 const button = document.getElementById("tombol");
 
-const output = document.getElementById('output');
+const output = document.getElementById("output");
+
+const daftarNama = [];
 
 function onClick() {
-  const isiInput = input.value;
+  const namaBaru = input.value;
+  daftarNama.push(namaBaru);
 
-  output.innerHTML = isiInput;
+  let html = '';
+  html += '<ul>';
+  for (let i = 0; i < daftarNama.length; i++) {
+    const nama = daftarNama[i];
+    html += '<li>' + nama + '</li>';
+  }
+  html += '</ul>';
+
+  output.innerHTML = html;
 }
 button.addEventListener("click", onClick);
